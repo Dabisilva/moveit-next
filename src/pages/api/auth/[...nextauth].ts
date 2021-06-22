5ab43ef63566d5ce0ed3614fad6db2d4ebe107fa
Davi <dabisilvaond@gmail.com>
2021-06-22 15:46:56 -0300

    changing variables env

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,13 +5,13 @@ import { api } from "../../../services/api";
 export default NextAuth({
   providers: [
     Providers.GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID_PRODUCTION,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET_PRODUCTION,
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: "read:user",
     }),
     Providers.Google({
-      clientId: process.env.GOOGLE_CLIENT_ID_PRODUCTION,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET_PRODUCTION,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   callbacks: {
